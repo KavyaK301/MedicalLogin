345c1cc3c4321820596a046310941a0ff44a1f35
innocentkishore <smartkishorekumar57@gmail.com>
2025-07-17 10:37:20 +0530

    reception-add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home"; // Make sure this path is correct
-
+import Reception from "./components/Reception/Reception";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/reception/*" element={<Reception/>}></Route>
       </Routes>
     </Router>
   );
