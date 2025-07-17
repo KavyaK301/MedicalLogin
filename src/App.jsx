1e21f113d962fcf23a885663e83618b69e93cc8d
Anushiniananth <anushiniananth@gmail.com>
2025-07-17 11:32:55 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home"; // Make sure this path is correct
+import PatientProfile from "./components/PatientProfile/PatientProfile"; 
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/patient-profile" element={<PatientProfile />} />
       </Routes>
     </Router>
   );
